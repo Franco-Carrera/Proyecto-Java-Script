d5c4cc3850a0173b3f69bd029c9b743d6b7df7f6
Franco-Carrera <carrerafranco825@gmail.com>
2021-06-02 18:19:07 -0300

    switch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//Validando inputs del usuario
 function valida() {
     let nombre = document.getElementById ('nombre').value;
     let radios = document.getElementsByName ('comprar');
@@ -20,7 +20,7 @@ function valida() {
 
  console.log ('Nombre: ' + nombre + ' Compras: ' +valorRadio + ' Es deportista: ' + deportista );
 }
-
+//Limpiandolos
 function limpiar() {
    let nombre = document.getElementById ('nombre').value = '';
    let radios = document.getElementsByName ('comprar');
@@ -35,55 +35,150 @@ function limpiar() {
    deportista.checked = false;
 
 }
-//_______________________________________________________
+///_______________________Fin formulario______________________
 
 
- const listaOrdenadores = [];
- let misOrdenadores = ('Ingrese la cantidad de compus que desee comprar');
+//Array productos principales
+
+ const listaProductos = [];
+ let misProductos = ('Ingrese la cantidad de artículos que desee comprar');
     
-
-const crearListaDeOrdenadores = function (cantidad) {
+const crearListaDeProductos = function (cantidad) {
   for (let i = 1; i <= cantidad; i++){
-      const ordenador = [];
-      let nombreOrdenador = prompt (`Ingrese el nombre del ordenador nº ${i}`);
-      ordenador.push (nombreOrdenador);
-      let numeroOrdenador = parseInt (prompt(`Ingrese el numero de su ordenador ${nombreOrdenador}`));
-       listaOrdenadores.push (ordenador);
+      const producto = [];
+      let nombreProducto = prompt (`Ingrese el nombre del producto nº ${i}`);
+      producto.push (nombreProducto);
+      let numeroProducto = parseInt (prompt(`Ingrese el numero de su producto ${nombreProducto}`));
+       listaProductos.push (producto);
        
   }
-  for (ordenadores in listaOrdenadores) {nombre = listaOrdenadores [ordenadores] + '<br>';
+  for (productos in listaProductos) {
+    nombre = listaProductos [productos] + '<br>';
     document.write (nombre);
     
   } 
 }  
-   crearListaDeOrdenadores (misOrdenadores);
-  
- console.log (listaOrdenadores); 
-  console.log (misOrdenadores);
+   crearListaDeProductos (misProductos);
+   console.log (misProductos);
+
+//Como puedo interactuar en mi página con este array(?)
 //  alert ('Ha realizado su pago con exito');
 
- const compus = [
- {
-     nombre: 'FranPc',
-     procesador: 'I3 novena generacion',
-     ram: '32GB',
-     espacio: '1TB', nro:1
-   
-  },
 
- {
-     nombre: 'GuillePc',
-     procesador: 'Ryzen5 3600',
-    ram: '16GB',
-     espacio: '500GB', nro:2
- },
+ // Registro de productos 
+    const productos = [ 
+      {
+        id: 1,
+        nombre: 'Ketpler',
+        stock: 'Hay',
+        peso: '10kg'
+      },
 
- {     nombre: 'RandomPc',
-         procesador: 'I7 novena generacion',
-        ram: '32GB',
-        espacio: '2TB', nro:3
- }
+      {
+       id: 2,
+       nombre: 'Pelota goma',
+       stock: 'Hay',
+       peso: '800 gramos'
+     },
+     
+     {
+       id: 3,
+       nombre: 'Mancuernas',
+       stock: 'Hay',
+     peso: '12kg'
+     },
 
- ];
- console.log (compus);
+      {
+       id: 4,
+       nombre: 'Rueda',
+       stock: 'No Hay',
+       peso: '1kg'
+     }
+
+     ];
  
+//_________________________________________________
+   //Para ordenar atributos de nuevos productos
+   class Product {
+       constructor (nombre, precio) {
+         this.nombre = nombre;
+         this.precio = parseFloat (precio);
+         this.vendido = false;
+       }
+     }
+
+  function sortProducts (array,atribute,type) { 
+
+  // copia del arrray  
+      const products = array.slice();
+
+   //tipo de ordenamiento  
+     switch (type) {
+
+       case 'ASC':
+      //ascendente
+      return products.sort((p1,p2) => p1[atribute] >p2 [atribute]?1:-1);
+     
+       case 'DESC':
+       //descendente
+       return products.sort((p1,p2) => p2[atribute] >p1[atribute] ? 1:-1);
+       
+       default:
+         return products;
+      }  
+  }  
+
+   const ejemploProductos = [];
+
+  //utilizo clase Product
+ ejemploProductos.push (new Product('producto-2', '900'));
+ ejemploProductos.push (new Product('producto-3', '450'));
+ ejemploProductos.push (new Product('producto-1', '700')) 
+
+   const productosAscendente = sortProducts (ejemploProductos, 'precio', 'ASC');
+    console.log (productosAscendente);
+///__________________________________________________________
+
+    
+ //For each para.. llamar elemento específico de [{productos}]
+  productos.forEach ((elemento) => { 
+    if (typeof elemento == 'object') {
+     console.log  (elemento.stock)
+    }
+     console.log (elemento);
+  });  
+
+     //Como mostrar al cliente si hay o no stock en página
+
+
+  //saludando    
+  function saludar () {
+     this.name = 'Estos son mis productos';
+    
+  }
+ const ObjetoSaludar =  new saludar();
+  console.log (ObjetoSaludar.name)
+
+
+//______________________________________________
+
+let productosEnTexto = JSON.stringify (productos);
+ // console.log (productosEnTexto);
+
+localStorage.expone = productosEnTexto;
+ //console.log
+(JSON.parse(productosEnTexto));
+
+ let expone = [];
+ let productosParseado = JSON.parse (localStorage.expone); 
+
+function agregarAlCarrito(producto) {
+  expone.push (producto);
+  localStorage.expone = JSON.stringify (expone);
+}
+ agregarAlCarrito(10);
+ agregarAlCarrito(50);
+
+ console.log (expone);
+
+ //___________________________________________
--- tabla.js
+++ tabla.js
@@ -1,74 +1,73 @@
 
      // ELEMENTOS HTML
-     const tabla = document.getElementById("tabla-personas");
+     const tabla = document.getElementById("tabla-elementos");
 
 
      // REGISTRO DE PERSONAS 
-     const personas = [
+     const elementos = [
         {
              id: 1,
-             nombre: "Guy",
-             apellido: "Rose",
-             email: "guy.rose@example.com",
+             nombre: "Pesas kettlebell ",
+             Stock: "disponible",
+             función: "entrenamiento muscular",
              direccion: "2668 Ranchview Dr"
          },
            
          {
              id: 2,
-             nombre: "Ritthy",
-             apellido: "Hill",
-             email: "ritthy.hill@example.com",
+             nombre: "Pelota de yoga",
+             stock: "disponible",
+             función: " flexibilidad y equilibrio",
              direccion: "9117 Oak Ridge Ln"
          },
             
          {
              id: 3,
-             nombre: "Derrick",
-             apellido: "Hanson",
-             email: "derrick.hanson@example.com",
+             nombre: "Mancuernas ",
+             stock: "disponible",
+             función: "fuerza muscular",
              direccion: "4240 Photinia Ave"
          },
      
          {
              id: 4,
-             nombre: "Ted",
-             apellido: "Peters",
-             email: "ted.peters@example.com",
+             nombre: "Rueda abdominal",
+             stock: "Intermedio",
+             función: "desarrollo de abdomen",
              direccion: "2976 Valley View Ln"
          },
      
          {
              id: 5,
-             nombre: "Ted",
-             apellido: "Peters",
-             email: "ted.peters@example.com",
+             nombre: "Step",
+             stock: "Intermedio",
+             función: "desarrollo de glúteos",
              direccion: "2995 Edwards Rd"
-         },
+         }
      
-        {
-              id: 6,
-              nombre: 'Fausto',
-              apellido: 'Carmesí',
-              email: 'Fauscarmesí@them.com',
-              direccion: 'West Side Records'
-         } 
-     
-     ]; //_/agregamos ;
+        // {
+        //       id: 6,
+        //       nombre: 'Fausto',
+        //       apellido: 'Carmesí',
+        //       email: 'Fauscarmesí@them.com',
+        //       direccion: 'West Side Records'
+        //  } 
+     ];  
      
      
      // FUNCIONES DEL DOM
-     function insertarFila(tabla, persona) {
+     function insertarFila(tabla, elemento) {
      
          const fila = document.createElement("tr");
          
-         for (const atributo in persona) {
+         for (const atributo in elemento) {
      
              // Creamos una celda nueva
              const celda = document.createElement("td");
              celda.style.color = 'grey';                //+ color
      
              // Asignamos el valor del atributo como contenido textual dentro de la celda.
-             celda.textContent = persona[atributo]; 
+             celda.textContent = elemento[atributo]; 
              // Usamos la notación corchetes para acceder al atributo ya que es una variable.
      
              // Insertamos la celda a nuestra fila.
@@ -83,8 +82,18 @@
          
      
      // Recorremos el array y las mostramos en HTML
-     for (const persona of personas) {
-         insertarFila(tabla, persona);
+     for (const elemento of elementos) {
+         insertarFila(tabla, elemento);
      }
      //______________________________________________
      
+
+    // declare a variables first
+const $clickMeElement = $('.galeria-port').eq(0);
+ 
+// use this variable
+$clickMeElement.on( "click", function() {
+    alert("Estos son productos originales");
+});
+
+//Desarrollo de DOM y eventos
